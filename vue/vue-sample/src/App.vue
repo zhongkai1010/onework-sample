35b7147b3635368374257ce44549266f82b640f2
zhongkai <zhongkai1010@163.com>
2022-01-06 21:24:21 +0800

    登录示例

--- vue/vue-sample/src/App.vue
+++ vue/vue-sample/src/App.vue
@@ -1,54 +1,84 @@
- 
-
+<!--
+ * @Author: 钟凯
+ * @Date: 2022-01-06 19:26:45
+ * @LastEditTime: 2022-01-06 21:18:25
+ * @LastEditors: 钟凯
+ * @Description: 
+ * @FilePath: \vue-sample\src\App.vue
+ * 可以输入预定的版权声明、个性签名、空行等
+-->
 <template>
-  <div class="common-layout">
-    <el-container>
-      <el-header>Header</el-header>
-      <el-main>Main</el-main>
-      <el-footer>Footer</el-footer>
-    </el-container>
+  <div class="login_box">
+    <el-form
+      :model="loginData"
+      :rules="formRules"
+      ref="loginForm"
+      label-width="120px"
+    >
+      <el-form-item label="用户名" prop="username">
+        <el-input
+          v-model="loginData.username"
+          placeholder="请输入用户名"
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input
+          type="password"
+          v-model="loginData.password"
+          placeholder="请输入密码"
+        ></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="login()" type="primary" style="width: 200px">
+          登录
+        </el-button>
+        <el-button type="text" style="margin-left: 60px">找回密码</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
+import { ElNotification } from "element-plus";
+
 export default {
   name: "App",
+  data() {
+    return {
+      loginData: {
+        username: "",
+        password: "",
+      },
+      formRules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { message: "密码长度必须大于6位", min: 6, trigger: "blur" },
+        ],
+      },
+    };
+  },
+  methods: {
+    login() {
+      this.$refs["loginForm"].validate((valid) => {
+        if (valid) {
+          ElNotification({
+            title: "Success",
+            message: "This is a success message",
+            type: "success",
+          });
+        }
+      });
+    },
+  },
 };
 </script>
 
 <style>
-.el-header,
-.el-footer {
-  background-color: #b3c0d1;
-  color: var(--el-text-color-primary);
-  text-align: center;
-  line-height: 60px;
-}
-
-.el-aside {
-  background-color: #d3dce6;
-  color: var(--el-text-color-primary);
-  text-align: center;
-  line-height: 200px;
-}
-
-.el-main {
-  background-color: #e9eef3;
-  color: var(--el-text-color-primary);
-  text-align: center;
-  line-height: 160px;
-}
-
-body > .el-container {
-  margin-bottom: 40px;
-}
-
-.el-container:nth-child(5) .el-aside,
-.el-container:nth-child(6) .el-aside {
-  line-height: 260px;
-}
-
-.el-container:nth-child(7) .el-aside {
-  line-height: 320px;
+.login_box {
+  padding: 10px;
+  width: 450px;
 }
 </style>
--- vue/vue-sample/src/main.js
+++ vue/vue-sample/src/main.js
@@ -1,6 +1,16 @@
+/*
+ * @Author: 钟凯
+ * @Date: 2022-01-06 19:26:45
+ * @LastEditTime: 2022-01-06 19:38:36
+ * @LastEditors: 钟凯
+ * @Description: 
+ * @FilePath: \vue-sample\src\main.js
+ * 可以输入预定的版权声明、个性签名、空行等
+ */
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import './assets/css/base.css'
 import App from './App.vue'
 
 
