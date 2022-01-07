06bc732d48e40f52eece5f163c1e74443908925a
zhongkai1010 <zhongkai1010@163.com>
2022-01-07 15:36:04 +0800

    加入vue-router

--- vue/vue-sample/src/App.vue
+++ vue/vue-sample/src/App.vue
@@ -1,84 +1,22 @@
-<!--
- * @Author: 钟凯
- * @Date: 2022-01-06 19:26:45
- * @LastEditTime: 2022-01-06 21:18:25
- * @LastEditors: 钟凯
- * @Description: 
- * @FilePath: \vue-sample\src\App.vue
- * 可以输入预定的版权声明、个性签名、空行等
--->
-<template>
-  <div class="login_box">
-    <el-form
-      :model="loginData"
-      :rules="formRules"
-      ref="loginForm"
-      label-width="120px"
-    >
-      <el-form-item label="用户名" prop="username">
-        <el-input
-          v-model="loginData.username"
-          placeholder="请输入用户名"
-        ></el-input>
-      </el-form-item>
-      <el-form-item label="密码" prop="password">
-        <el-input
-          type="password"
-          v-model="loginData.password"
-          placeholder="请输入密码"
-        ></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button @click="login()" type="primary" style="width: 200px">
-          登录
-        </el-button>
-        <el-button type="text" style="margin-left: 60px">找回密码</el-button>
-      </el-form-item>
-    </el-form>
-  </div>
+ <template>
+  <h1>Hello App!</h1>
+  <p>
+    <!--使用 router-link 组件进行导航 -->
+    <!--通过传递 `to` 来指定链接 -->
+    <!--`<router-link>` 将呈现一个带有正确 `href` 属性的 `<a>` 标签-->
+    <router-link to="/">Go to Home</router-link>
+    <router-link to="/login">Go to Login</router-link>
+  </p>
+  <!-- 路由出口 -->
+  <!-- 路由匹配到的组件将渲染在这里 -->
+  <router-view></router-view>
 </template>
 
 <script>
-import { ElNotification } from "element-plus";
-
 export default {
   name: "App",
   data() {
-    return {
-      loginData: {
-        username: "",
-        password: "",
-      },
-      formRules: {
-        username: [
-          { required: true, message: "请输入用户名", trigger: "blur" },
-        ],
-        password: [
-          { required: true, message: "请输入密码", trigger: "blur" },
-          { message: "密码长度必须大于6位", min: 6, trigger: "blur" },
-        ],
-      },
-    };
-  },
-  methods: {
-    login() {
-      this.$refs["loginForm"].validate((valid) => {
-        if (valid) {
-          ElNotification({
-            title: "Success",
-            message: "This is a success message",
-            type: "success",
-          });
-        }
-      });
-    },
+    return {};
   },
 };
 </script>
-
-<style>
-.login_box {
-  padding: 10px;
-  width: 450px;
-}
-</style>
--- vue/vue-sample/src/main.js
+++ vue/vue-sample/src/main.js
@@ -3,17 +3,49 @@
  * @Date: 2022-01-06 19:26:45
  * @LastEditTime: 2022-01-06 19:38:36
  * @LastEditors: 钟凯
- * @Description: 
+ * @Description:
  * @FilePath: \vue-sample\src\main.js
  * 可以输入预定的版权声明、个性签名、空行等
  */
-import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import './assets/css/base.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import { createStore } from 'vuex';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import './assets/css/base.css';
+import App from './App.vue';
+import Index from './views/Index.vue';
+import Login from './views/Login.vue';
 
+// 2. 定义一些路由
+// 每个路由都需要映射到一个组件。
+// 我们后面再讨论嵌套路由。
+const routes = [
+    { path: '/', component: Index },
+    { path: '/login', component: Login },
+];
 
-createApp(App)
-    .use(ElementPlus)
-    .mount('#app')
+// 3. 创建路由实例并传递 `routes` 配置
+// 你可以在这里输入更多的配置，但我们在这里
+// 暂时保持简单
+const router = createRouter({
+    // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
+    history: createWebHistory(),
+    routes, // `routes: routes` 的缩写
+});
+
+const store = createStore({
+    state() {
+        return {
+            count: 0,
+        };
+    },
+    mutations: {
+        increment(state) {
+            state.count++;
+        },
+    },
+});
+
+createApp(App).use(store).use(router).use(ElementPlus)
+    .mount('#app');
