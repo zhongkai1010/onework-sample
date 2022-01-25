310bb7d8a3c65c4de31ea30041ff6d79e0495658
zhongkai1010 <zhongkai1010@163.com>
2022-01-25 17:30:56 +0800

    加入Spring Security 示例

---
+++ java/onework-sample-spring-security/src/main/java/onework/sample/springsecuritysample/OneworkSampleSpringSecurityApplication.java
@@ -0,0 +1,17 @@
+package onework.sample.springsecuritysample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Administrator
+ */
+@SpringBootApplication
+public class OneworkSampleSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneworkSampleSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ java/onework-sample-spring-security/src/test/java/onework/sample/springsecuritysample/OneworkSampleSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package onework.sample.springsecuritysample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneworkSampleSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
