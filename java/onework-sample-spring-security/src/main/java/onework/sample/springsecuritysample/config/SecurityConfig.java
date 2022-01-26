8178969429b0e7900b1f1a70b7f6c59a2ac96a8c
zhongkai1010 <zhongkai1010@163.com>
2022-01-26 17:30:22 +0800

    添加Spring Security的WebSecurityConfigurerAdapter类

---
+++ java/onework-sample-spring-security/src/main/java/onework/sample/springsecuritysample/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package onework.sample.springsecuritysample.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author : zhongkai1010@163.com
+ * @version V1.0
+ * @Project: onework-sample-spring-security
+ * @Package onework.sample.springsecuritysample.config
+ * @Description: 描述
+ * @date Date : 2022年01月26日 15:57
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // @formatter:off
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests((requests) -> requests.anyRequest().authenticated());
+        http.formLogin();
+        http.httpBasic();
+    }
+    // @formatter:on
+}
