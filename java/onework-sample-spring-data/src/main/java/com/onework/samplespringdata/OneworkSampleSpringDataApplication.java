f113e41d244e8454377eed0b083988f25238a8a7
zhongkai1010 <zhongkai1010@163.com>
2022-02-08 17:59:18 +0800

    加入spring-data-redis示例

--- java/onework-sample-spring-data/src/main/java/com/onework/samplespringdata/OneworkSampleSpringDataApplication.java
+++ java/onework-sample-spring-data/src/main/java/com/onework/samplespringdata/OneworkSampleSpringDataApplication.java
@@ -2,6 +2,7 @@ package com.onework.samplespringdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  */
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching
 public class OneworkSampleSpringDataApplication {
 
     public static void main(String[] args) {
