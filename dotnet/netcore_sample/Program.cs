d7d69738120007eabb3dc24c9cc6092c1d0c8d4d
zhongkai1010 <zhongkai1010@163.com>
2022-02-11 18:01:48 +0800

    添加.net core配置示例

--- dotnet/netcore_sample/Program.cs
+++ dotnet/netcore_sample/Program.cs
@@ -5,77 +5,185 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace OneWork
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            #region 依赖注入
 
-        public static IHostBuilder CreateHostBuilder(string[] args)
+            //IHostBuilder hostBuilder = Host.CreateDefaultBuilder(args);
+            //hostBuilder.ConfigureServices((hostBuilderContext, serviceCollection) =>
+            //{
+            //    serviceCollection.AddTransient<IRepository>(provider => new UserRepository());
+            //});
+            //IHost host = hostBuilder.Build();
+
+            //IServiceScope serviceScope = host.Services.CreateScope();
+            //IRepository repository = serviceScope.ServiceProvider.GetRequiredService<IRepository>();
+            //repository.Add();
+
+            //host.Run();
+
+            #endregion
+
+            #region .NET配置 基本示例
+
+            //IConfiguration config = new ConfigurationBuilder()
+            //    .AddJsonFile("appsettings.json")
+            //    .AddEnvironmentVariables()
+            //    .Build();
+
+            //Settings settings = config.GetRequiredSection("Settings").Get<Settings>();
+
+            //// Write the values to the console.
+            //Console.WriteLine($"KeyOne = {settings.KeyOne}");
+            //Console.WriteLine($"KeyTwo = {settings.KeyTwo}");
+            //Console.WriteLine($"KeyThree:Message = {settings.KeyThree.Message}");
+
+            #endregion
+
+            #region .NET配置 使用托管的基本示例
+
+            //using IHost host = Host.CreateDefaultBuilder(args).Build();
+
+            //IConfiguration config = host.Services.GetRequiredService<IConfiguration>();
+
+            //int keyOneValue = config.GetValue<int>("KeyOne");
+            //bool keyTwoValue = config.GetValue<bool>("KeyTwo");
+            //string keyThreeNestedValue = config.GetValue<string>("KeyThree:Message");
+
+            //Console.WriteLine($"KeyOne = {keyOneValue}");
+            //Console.WriteLine($"KeyTwo = {keyTwoValue}");
+            //Console.WriteLine($"KeyThree:Message = {keyThreeNestedValue}");
+
+            //await host.RunAsync();
+
+            #endregion
+
+            #region JSON 配置提供程序
+
+            //using IHost host = Host.CreateDefaultBuilder(args)
+            //    .ConfigureAppConfiguration((hostingContext, configuration) =>
+            //    {
+            //        configuration.Sources.Clear();
+
+            //        IHostEnvironment env = hostingContext.HostingEnvironment;
+
+            //        configuration
+            //            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            //            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true);
+
+            //        IConfigurationRoot configurationRoot = configuration.Build();
+
+            //        TransientFaultHandlingOptions options = new TransientFaultHandlingOptions();
+            //        configurationRoot.GetSection(nameof(TransientFaultHandlingOptions))
+            //            .Bind(options);
+
+            //        Console.WriteLine($"TransientFaultHandlingOptions.Enabled={options.Enabled}");
+            //        Console.WriteLine($"TransientFaultHandlingOptions.AutoRetryDelay={options.AutoRetryDelay}");
+            //    })
+            //    .Build();
+
+            //await host.RunAsync();
+
+            #endregion
+
+            #region 环境变量配置提供程序
+
+            //using IHost host = Host.CreateDefaultBuilder(args).Build();
+
+            //IConfiguration config = host.Services.GetRequiredService<IConfiguration>();
+
+            //string secretKey = config.GetValue<string>("SecretKey");
+
+            //Console.WriteLine($"KeyOne = {secretKey}");
+
+            //return Task.CompletedTask;
+
+            #endregion
+
+            #region .NET 中的选项模式
+
+            //using IHost host = Host.CreateDefaultBuilder(args)
+            //    .ConfigureContainer()
+            //    .ConfigureServices((context, services) =>
+            //    {
+                    
+            //    })
+            //    .Build();
+
+            // host.Run();
+
+            #endregion
+
+            #region .NET 依赖注入方式的选项模式
+
+            //using IHost host = Host.CreateDefaultBuilder(args)
+            //    .ConfigureServices((context, services) =>
+            //    {
+            //        services.Configure<TransientFaultHandlingOptions>(
+            //            context.Configuration.GetSection("TransientFaultHandlingOptions"));
+            //    })
+            //    .Build();
+
+            //TransientFaultHandlingOptions options =
+            //    host.Services.GetRequiredService<IOptions<TransientFaultHandlingOptions>>().Value;
+            //Console.WriteLine($"Enabled={options.Enabled}");
+            //Console.WriteLine($"AutoRetryDelay={options.AutoRetryDelay}");
+
+            //host.Run();
+
+            #endregion
+
+
+            #region 通用主机
+
+            #endregion
+        }
+    }
+
+
+    public interface IRepository
+    {
+        void Add();
+    }
+
+    public abstract class BaseRepository : IRepository
+    {
+        public void Add()
         {
-            IHostBuilder hostBuilder = Host.CreateDefaultBuilder(args);
-
-            /**
-             * 主机配置
-             * 
-             **/
-            hostBuilder.ConfigureHostConfiguration((configurationBuilder) =>
-            {
-                configurationBuilder.SetBasePath(Directory.GetCurrentDirectory());
-                configurationBuilder.AddJsonFile("appsettings.json", optional: false);
-                configurationBuilder.AddEnvironmentVariables(prefix: "PREFIX_");
-                configurationBuilder.AddCommandLine(args);
-            });
-
-            /**
-             * 应用配置
-             * 
-             **/
-            hostBuilder.ConfigureAppConfiguration((hostingContext, configurationBuilder) =>
-            {
-
-                IHostEnvironment env = hostingContext.HostingEnvironment;
-
-                Console.WriteLine("hostBuilder.ConfigureAppConfiguration,env:{0}", env.EnvironmentName);
-            });
-
-            /**
-             * 日志配置
-             * 
-             **/
-            hostBuilder.ConfigureLogging((hostContext, builder) =>
-            {
-                IConfigurationSection configurationSection = hostContext.Configuration.GetSection("enableConsoleJson");
-                if (configurationSection.Value == "true")
-                {
-                    builder.AddJsonConsole(options =>
-                    {
-                        options.IncludeScopes = false;
-                        options.TimestampFormat = "hh:mm:ss ";
-                        options.JsonWriterOptions = new JsonWriterOptions
-                        {
-                            Indented = true
-                        };
-                    });
-                }
-            });
-
-            /**
-             * 框架提供的服务
-             * 
-             **/
-            hostBuilder.ConfigureServices((hostContext, services) =>
-            {
-                services.AddHostedService<CustomHostedService>();
-
-                Console.WriteLine("hostBuilder.ConfigureServices");
-            });
-
-            return hostBuilder;
+            Console.WriteLine("BaseRepository_Add");
         }
+    }
+
+
+    public class UserRepository : IRepository
+    {
+        public void Add()
+        {
+            Console.WriteLine("UserRepository_Add");
+        }
+    }
+
+    public class TransientFaultHandlingOptions
+    {
+        public bool Enabled { get; set; }
+        public TimeSpan AutoRetryDelay { get; set; }
+    }
+
+    public class Settings
+    {
+        public int KeyOne { get; set; }
+        public bool KeyTwo { get; set; }
+        public NestedSettings KeyThree { get; set; } = null!;
+    }
+
+    public class NestedSettings
+    {
+        public string Message { get; set; } = null!;
     }
 }
