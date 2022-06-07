7692c302eb092dea2e83ec64a6c0c7d47aceca0e
zhongkai1010 <zhongkai1010@163.com>
2022-06-07 17:40:23 +0800

    webpack示例

---
+++ web/onework-site/src/index.js
@@ -0,0 +1,19 @@
+import _ from "lodash";
+import "./style.css";
+import Icon from "./logo.png";
+
+function component() {
+  var element = document.createElement("div");
+
+  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  var myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
+
+  return element;
+}
+
+document.body.appendChild(component());
