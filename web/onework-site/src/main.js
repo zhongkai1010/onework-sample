15dc319a0c3267d0210df37180c76cefee384120
zhongkai1010 <zhongkai1010@163.com>
2022-06-08 13:57:54 +0800

    webpack热更新和scss

---
+++ web/onework-site/src/main.js
@@ -0,0 +1,4 @@
+import _ from "lodash";
+import "../src/style/main.scss";
+import '../index.html'
+ 
--- web/onework-site/webpack.config.js
+++ web/onework-site/webpack.config.js
@@ -1,10 +1,12 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/main.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   module: {
     rules: [
@@ -13,9 +15,49 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"],
+        test: /\.less$/,
+        use: ["style-loader", "css-loader", "less-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.styl$/,
+        use: ["style-loader", "css-loader", "stylus-loader"],
+      },
+      {
+			  test: /\.html$/,
+        loader: 'html-loader'
+			},
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              esModule: false  //很重要，否则<img ur="[object Module]">依然无法显示图片
+            }
+          },
+        ],
+        type: 'javascript/auto'
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // 以 public/index.html 为模板创建文件
+      // 新的html文件有两个特点：1. 内容和源文件一致 2. 自动引入打包生成的js等资源
+      template: 'index.html'
+    }),
+  ],
+  // 开发服务器
+  devServer: {
+    host: "localhost", // 启动服务器域名
+    port: "3001", // 启动服务器端口号
+    open: true, // 是否自动打开浏览器
+    hot: true,
+  },
+  mode: "development",
 };
