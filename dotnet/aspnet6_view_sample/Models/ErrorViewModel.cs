dbc7bd66bfd874c4faaf1c18f3ac43ae0b3b9338
zhongkai1010 <zhongkai1010@163.com>
2022-07-05 10:03:59 +0800

    Razor

---
+++ dotnet/aspnet6_view_sample/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace aspnet6_view_sample.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ dotnet/aspnet6_view_sample/ViewComponents/PriorityListViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet6_view_sample.ViewComponents;
+
+public class PriorityListViewComponent : ViewComponent
+{
+    public Task<IViewComponentResult> InvokeAsync(dynamic data)
+    {
+        return Task.FromResult<IViewComponentResult>(View(data));
+    }
+
+    public Task<IViewComponentResult> Test()
+    {
+        return Task.FromResult<IViewComponentResult>(View());
+    }
+}
