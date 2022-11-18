03a6095a47e8521ba4d9247bc8c8a368692c2092
zhongkai1010 <zhongkai1010@163.com>
2022-11-18 14:25:17 +0800

    mongodb对json处理

--- dotnet/netcore6_mongodb/Program.cs
+++ dotnet/netcore6_mongodb/Program.cs
@@ -8,6 +8,7 @@ public class Person
     public ObjectId Id { get; set; }
     public string Name { get; set; }
     public BsonDocument Properties { get; set; }
+
     public ICollection<PersonJob> Jobs { get; set; }
 }
 
@@ -22,18 +23,18 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        // Replace the uri string with your MongoDB deployment's connection string.
         MongoClient client = new MongoClient("mongodb://localhost:27017");
         IMongoDatabase database = client.GetDatabase("foo");
         IMongoCollection<Person> collection = database.GetCollection<Person>("bar");
-
         Person person = new Person
         {
             Id = ObjectId.GenerateNewId(),
             Name = "test",
             Properties = new BsonDocument
             {
-                {"age", BsonValue.Create(30)},
-                {"birthday", BsonString.Create("2020-1-1")},
+                {"age", 30},
+                {"birthday", "2020-1-1"},
                 {"tags", BsonArray.Create(new[] {"a", "b"})},
             },
             Jobs = new List<PersonJob>
@@ -43,7 +44,26 @@ internal class Program
             }
         };
 
+        // List<PersonJob> data1 = new List<PersonJob>
+        // {
+        //     new() {Name = "work1", Value = "1"},
+        //     new() {Name = "work2", Value = "2"}
+        // };
+        // string json1 = Newtonsoft.Json.JsonConvert.SerializeObject(data1);
+        // BsonArray doc1 = MongoDB.Bson.Serialization.BsonSerializer.Deserialize<BsonArray>(json1);
+        // person.Properties.Add("doc1", doc1);
+        //
+        // PersonJob data2 = new() {Name = "work1", Value = "1"};
+        // string json2 = Newtonsoft.Json.JsonConvert.SerializeObject(data2);
+        // BsonDocument doc2 = MongoDB.Bson.Serialization.BsonSerializer.Deserialize<BsonDocument>(json2);
+        // person.Properties.Add("doc2", doc2);
+ 
+        string value = "{}";
+        BsonDocument doc3 = MongoDB.Bson.Serialization.BsonSerializer.Deserialize<BsonDocument>(value);
+ 
+        person.Properties = doc3;
         collection.InsertOne(person);
+        
         Console.WriteLine("Hello World!");
     }
 }
