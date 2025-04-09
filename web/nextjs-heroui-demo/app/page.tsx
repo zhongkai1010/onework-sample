265004b0c21f3f8d08405a44261d27f1640a50df
zhongkai <zhongkai1010@163.com>
2025-04-09 15:05:02 +0800

    feat:新增next示例

--- web/nextjs-heroui-demo/app/page.tsx
+++ web/nextjs-heroui-demo/app/page.tsx
@@ -1,29 +1,35 @@
-import logoSvg from "@/public/logo.svg";
 import Image from "next/image";
-import CategoryDropdownButton from '@/components/CategoryDropdownButton'
-import SwitchLayoutButton from '@/components/SwitchLayoutButton'
-import SwitchThemeButton from '@/components/SwitchThemeButton'
+import { Button } from "@heroui/button";
+
+import logoSvg from "@/public/next.svg";
 
 export default function Home() {
   return (
     <div className="flex h-screen w-screen flex-col bg-[#f4f5f5]">
       <div className="flex h-[54] flex-row bg-[#ffffff] pr-[24] pl-[24]">
-        <Image src={logoSvg} alt="" className="mr-[16]" />
+        <Image alt="" className="mr-[16]" src={logoSvg} />
         <div className="flex flex-row items-center justify-center">
-          <a className="mr-[24] ml-[24] cursor-pointer text-base text-slate-400 hover:text-slate-300">
+          <a
+            className="mr-[24] ml-[24] cursor-pointer text-base text-slate-400 hover:text-slate-300"
+            href="https://www.imooc.com"
+          >
             课程
           </a>
-          <a className="mr-[24] ml-[24] cursor-pointer text-base text-slate-400 hover:text-slate-300">
+          <a
+            className="mr-[24] ml-[24] cursor-pointer text-base text-slate-400 hover:text-slate-300"
+            href="https://www.imooc.com"
+          >
             APP
           </a>
-          <a className="mr-[24] ml-[24] cursor-pointer text-base text-slate-400 hover:text-slate-300">
+          <a
+            className="mr-[24] ml-[24] cursor-pointer text-base text-slate-400 hover:text-slate-300"
+            href="https://www.imooc.com"
+          >
             会员
           </a>
         </div>
         <div className="ml-auto flex flex-row items-center justify-center">
-          <CategoryDropdownButton />
-          <SwitchLayoutButton />
-          <SwitchThemeButton />
+          <Button color="primary">Button</Button>
         </div>
       </div>
       <div className="h-[132] bg-[#1e80ff]">2</div>
