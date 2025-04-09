cb7efe64e90e8853474bde0b818ac63f2454379e
zhongkai <zhongkai1010@163.com>
2025-04-09 11:28:52 +0800

    feat:配置文件

--- web/nextjs-antd-demo/app/layout.tsx
+++ web/nextjs-antd-demo/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { AntdRegistry } from "@ant-design/nextjs-registry";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,12 +24,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AntdRegistry> {children}</AntdRegistry>
+        {children}
       </body>
     </html>
+
   );
 }
--- web/nextjs-antd-demo/app/page.tsx
+++ web/nextjs-antd-demo/app/page.tsx
@@ -1,103 +1,37 @@
+import logoSvg from "@/public/logo.svg";
 import Image from "next/image";
+import CategoryDropdownButton from '@/components/CategoryDropdownButton'
+import SwitchLayoutButton from '@/components/SwitchLayoutButton'
+import SwitchThemeButton from '@/components/SwitchThemeButton'
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+    <div className="flex h-screen w-screen flex-col bg-[#f4f5f5]">
+      <div className="flex h-[54] flex-row bg-[#ffffff] pr-[24] pl-[24]">
+        <Image src={logoSvg} alt="" className="mr-[16]" />
+        <div className="flex flex-row items-center justify-center">
+          <a className="mr-[24] ml-[24] cursor-pointer text-base text-slate-400 hover:text-slate-300">
+            课程
           </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
+          <a className="mr-[24] ml-[24] cursor-pointer text-base text-slate-400 hover:text-slate-300">
+            APP
+          </a>
+          <a className="mr-[24] ml-[24] cursor-pointer text-base text-slate-400 hover:text-slate-300">
+            会员
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="ml-auto flex flex-row items-center justify-center">
+          <CategoryDropdownButton />
+          <SwitchLayoutButton />
+          <SwitchThemeButton />
+        </div>
+      </div>
+      <div className="h-[132] bg-[#1e80ff]">2</div>
+      <div className="flex flex-1 flex-row bg-[#f4f5f5] p-[16]">
+        <div className="w-[321] bg-[#ffff]">3</div>1
+        <div className="mr-[16] ml-[16] flex-1 bg-[#ffff]">4</div>
+        <div className="w-[321] bg-[#ffff]">5</div>
+      </div>
     </div>
   );
 }
